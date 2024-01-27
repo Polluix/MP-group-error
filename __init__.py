187a4d32c1cf68aef6809221d6e7c3381fcd898c
Polluix <lipe311973@gmail.com>
2024-01-27 19:24:21 -0300

    dataframe with parameters and graphs

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
-from dash import Dash, html, dcc, Input, Output,callback
+from dash import Dash, dcc, Input, Output,callback, dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
 
@@ -40,11 +39,15 @@ dfs_19 = {
     'df_19x12':df_19x12
 }
 
+def dens(h:int) -> float: #[kg/m3] Densidade do ar a altitude h
+    H:float = 3.28*h
+    return ((1-6.875E-6*H)**5.2561)/(1-6.875E-6*H)*1.225
+
 #Thust(v) function for each set:
-def Thrust(v, *args, h=0):
+def Thrust(v, *args, h=1000):
     a, b, c, d = args
     
-    return (a*v**3 + b*v**2 + c*v + d)
+    return (a*v**3 + b*v**2 + c*v + d)*dens(h)/dens(0)
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
@@ -92,8 +95,6 @@ T.update_layout(
     }
 )
 
-
-
 props = ['16x10', '17x10', '19x12']
 
 dropdown_thrust = dcc.Dropdown(
@@ -103,23 +104,123 @@ dropdown_thrust = dcc.Dropdown(
     value=props[0]
 )
 
+#Sg for a propulsion set
+from src.flightlib import *
+from src.perflib import *
+
+dist_16 = []
+dist_17 = []
+dist_19 = []
+for j in range(3):
+    
+    tuplef = (coefs[j][0],coefs[j][1],coefs[j][2],coefs[j][3])
+
+    for i in range(2):
+        distance = Sg(Thrust, 1000, mi, m, 0, *tuplef)
+
+        if j!=2:
+            if j!=1:
+                dist_16.append(distance)
+            else: dist_17.append(distance)
+        else: dist_19.append(distance)
+
+        tuplef = (coefs[j+3][0],coefs[j+3][1],coefs[j+3][2],coefs[j+3][3])
+
+# [-0.004068575720411194, 0.04747268366289824, -0.8223437807712293, 37.64674777530596]
+# -0.00406858*v**3 + 0.04747268*v**2 + -0.82234378*v + 37.646747775305954
+
+#MTOW for a propulsion set
+tuplef = (coefs[0][0],coefs[0][1],coefs[0][2],coefs[0][3])
+mtow_16 = []
+mtow_17 = []
+mtow_19 = []
+
+for j in range(3):
+    tuplef = (coefs[j][0],coefs[j][1],coefs[j][2],coefs[j][3])
+    for i in range(2):
+        mtow = bissecao(MTOW_func, *tuplef, a=8, b=25, h=1000, wind=0)
+
+        if j!=2:
+            if j!=1:
+                mtow_16.append(mtow)
+            else: mtow_17.append(mtow)
+        else: mtow_19.append(mtow)
+
+        tuplef = (coefs[j+3][0],coefs[j+3][1],coefs[j+3][2],coefs[j+3][3])
+
+table = {
+    'Propeller': [
+            props[0],
+            props[0],
+            props[1],
+            props[1],
+            props[2],
+            props[2]
+        ],
+    'Takeoff distance [m]':[
+            dist_16[0],
+            dist_16[1],
+            dist_17[0],
+            dist_17[1],
+            dist_19[0],
+            dist_19[1],
+        ],
+    'MTOW [kg]':[
+        mtow_16[0],
+        mtow_16[1],
+        mtow_17[0],
+        mtow_17[1],
+        mtow_19[0],
+        mtow_19[1],
+    ],
+    'Static thrust [N]':[
+        coefs[0][-1],
+        coefs[3][-1],
+        coefs[1][-1],
+        coefs[4][-1],
+        coefs[2][-1],
+        coefs[5][-1],
+    ],
+    'Software': [
+        'Aerodesign Propeller Selector',
+        'Motocalc',
+        'Aerodesign Propeller Selector',
+        'Motocalc',
+        'Aerodesign Propeller Selector',
+        'Motocalc'
+    ]
+}
+tableframe = pd.DataFrame(table)
+
 row = dbc.Row(
     [
-        dbc.Col(
-            [
-                dropdown_thrust,
-                dcc.Graph(figure=T, id='thrust-fig'),
-            ]
-        ),
-        dbc.Col(
-            dcc.Graph(),
-        )
-    ]
+        dropdown_thrust,
+        dcc.Graph(figure=T, id='thrust-fig'),
+    ],
+    style={
+        'width':'60vw',
+    }
+)
+row2 = dbc.Row(
+    style={
+        'height':'15vh'
+    }
 )
 
-
 app.layout = dbc.Container(
-    row,
+    [
+        dbc.Row(
+            dash_table.DataTable(tableframe.to_dict('records'),
+                                 [{"name": i, "id": i} for i in tableframe.columns],
+                                 ),
+            style={
+                'width':'60vw',
+                'height':'20vh'
+            }
+        ),
+        row2,
+        row
+    ],
     fluid=True,
     style={'backgroundColor':'#092635'}
 )
@@ -138,18 +239,21 @@ def update_output(value):
         df = dfs_19
 
     T = go.Figure()
-    
+    v = np.linspace(0,50,30)
     index = props.index(value)
     dist = len(props)
-    tuplef = (coefs[index][0],coefs[index][1],coefs[index][2],coefs[0][3])
+    tuplef = (coefs[index][0],coefs[index][1],coefs[index][2],coefs[index][3])
     
+    software = 'Aerodesign Propeller Selector'
     for i in df:
         T = T.add_trace(go.Scatter(
                             x=v,
                             y=Thrust(v, *tuplef),
-                            name=i),
+                            name=software, 
+                            ),
                         )
         tuplef = (coefs[index+dist][0],coefs[index+dist][1],coefs[index+dist][2],coefs[index+dist][3])
+        software = 'Motocalc'
 
     T.update(
         layout_yaxis_range=[0,45],
@@ -160,7 +264,15 @@ def update_output(value):
             'plot_bgcolor':'rgba(0,0,0,0)',
             'paper_bgcolor':'rgba(0,0,0,0)',
             'font_color':'white'
-        }
+        },
+        yaxis_title="Thrust [N]",
+        xaxis_title="Velocity [m/s]",
+        title={
+        'text': "Dynamic Thrust",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
     )
     return T
 
