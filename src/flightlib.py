ffa3f00125dfde073a2532de8ad8bd7dd6abd586
Polluix <lipe311973@gmail.com>
2024-01-27 16:44:12 -0300

    propeller data and required functions

---
+++ src/flightlib.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 14:57:32 2022
+
+@author: Luiz
+"""
+import numpy as np
+from scipy.constants import g
+
+def dens(h:int) -> float: #[kg/m3] Densidade do ar a altitude h
+    H:float = 3.28*h
+    return ((1-6.875E-6*H)**5.2561)/(1-6.875E-6*H)*1.225
+
+# ---------------------------Constantes-----------------------------
+ground:int   = 0  # [m] Altitude do solo em relação ao nível do mar (Joinville = 4m)
+S:float      = 1.15#[m2] Área da asa
+v:np.ndarray = np.arange(3, 20, 0.1) #[m/s] range de velocidades
+rho0:float   = dens(ground) #[kg/m3] Densidade para solo de Joinville
+CD0:float    = 0.042 #[adim.] CD0 da aeronave, aerodinâmica
+K:float      = 0.073 #[adim.] Constante K da aeronave, aerodinãmica
+CLmax:float  = 1.9235 #[adim.] CL máximo na decolagem, aerodinâmica.
+m:float      = 13.9447 #[kg] Critério de massa máxima da aeronave para h = 600 m
+MTOW:float   = m #[kg] MTOW da aeronaveusando bisseção para h = 1000 m
+W:float      = MTOW*g #força peso
+W_S          = W/S #carga alar
+
+# ---------------------------Funções------------------------------
+def q(v:float,h:int) -> float: #[N/m2] Pressão dinâmica função de v e h
+    return 0.5*dens(h)*v**2
+
+def CL(v:float, h:int, W:float) -> float: #[adim.] Coeficiente de sustentação total da aeronave, função de v, h e weight
+        return  W/(q(v,h) * S)
+
+def CD(v:float,h:int, W:float) -> float: #[adim.] Coeficiente de arrasto total da aeronave, função de v, h e weight
+    return CD0 + K*CL(v,h,W)**2
+   
+def Td(v:float,h:int) -> float: #[N] Tração disponível da aeronave,  função de v e h
+    return (-0.00406858*v**3 + 0.04747268*v**2 + -0.82234378*v + 37.646747775305954)*dens(h)/rho0
+    # return (-0.007666317579360982*v**3+ 0.030973683321508697*v**2+ -0.9194989133250041*v+ 34.36362393162395)*dens(h)/rho0
+    # return (-0.002378537089326893*v**3+ 0.02453091279009248*v**2+ -0.739071505421641*v+ 35.0028350945495)
+    # return (-0.002373529943051733*v**3+ 0.023714566856950295*v**2+ -0.725324364950364*v+ 34.161817296996716)*dens(h)/rho0
+    # return (-0.0058191152562674375*v**3+ 0.039460173493538656*v**2+ -1.0618404685167253*v+ 44.800907357381064)*dens(h)/rho0
+    # return (-0.0054450022025105405*v**3+ 0.03554673419523681*v**2+ -1.0704566898824777*v+ 46.79801307007784)*dens(h)/rho0
+    
+
+# print(Td(11, 346))
+# Td = lambda v, h=1000: (32.938595 + -0.800094*v + 0.053774*v**2 + -0.010142*v**3)*dens(h)/dens(0)
+
+def Ct(v:float, h:int) -> float:
+    return Td(v, h)/(q(v,h) * S)
+
+def Pd(v:float,h:int) -> float: #[W] Potência disponível da aeronave,  função de v e h
+    return Td(v, h)*v
+
+def Treq(v:float,h:int, W:float) -> float: #[N] Tração requerida da aeronave,  função de v, h e weight
+        return CD(v,h,W)*q(v,h)*S
+
+def Preq(v:float,h:int, W:float) -> float: #[W] Potência requerida da aeronave,  função de v, h e weight
+    return Treq(v,h,W)*v
+
+def V_stall(h:int, m:float) -> float: #velocidade de estol da aeronave
+    W:float = m*9.81
+    return  np.sqrt(2*W/(dens(h)*CLmax*S))
+
+def atrito(h:int, m:int, ds:int) -> float:
+    return V_stall(h, m)**2/(2*g*ds)
+
+def gamma(v:float, h:int, W:float) -> float:
+    return np.arctan(1/(CL(v,h,W)/CD(v,h,W)))*180/np.pi #[graus] Retorna o ângulo de planeio
+
+def bissecao(f:callable, a:float, b:float, h:int, wind:int, tol = 1e-4) -> float:
+  """
+  Calcula a raiz de uma função utilizando o método da bissecção
+  
+  Recebe:
+    f       => Função para encontrar a raiz
+    a,b     => Intervalo de busca (b>a)
+    h       => altitude densidade
+    tol     => Tolerância a se alcançar (b-a)/2 < tol    
+  Retorna:
+    n    => Número de iterações
+    p    => Raiz
+    erro => Erro (b-a)/2  
+  """
+
+  n      = 0
+  p      = (a+b)/2.0
+  imag_a = f(a, h, wind)
+  imag_p = f(p, h, wind)
+  erro   = (b-a)/2.0
+      
+  while erro>tol:
+      if imag_a * imag_p < 0:
+          b = p
+      else:
+          a = p
+          imag_a = imag_p
+      
+      n     += 1
+      p      = (a+b)/2.0
+      erro   = (b - a)/2.0
+      imag_p = f(p, h, wind)
+
+  return p
---
+++ src/perflib.py
@@ -0,0 +1,77 @@
+"""
+Biblioteca com as funções utilizadas nos scripts de voo
+@author: Luiz
+"""
+import numpy as np
+from flightlib import *
+
+# ---------------------------Constantes-----------------------------
+CD_to              = 0.0604 #coeficiente de arrasto na decolagem
+CL_to              = 0.8317 #coeficiente de sustentação na decolagem
+mi:float           = 0.02 #coeficiente de atrito da aeronave
+h:int              = 1000 #altitude densidade
+M_array:np.ndarray = np.linspace(8, 15, 3) #massas utilizadas nos plots
+h_array:np.ndarray = np.linspace(0, h, 3) #altitudes utilizadas nos plots
+
+def RC(v:float, h:int, W:float) -> float: #[m/s] Razão de subida da aeronave, em função de v, h e weight
+    return (Pd(v,h)-Preq(v,h, W))/W
+
+def theta(v:float, h:int, W:float) -> float:
+    return np.arcsin(RC(v,h,W)/v)*180/np.pi #[graus] Retorna o ângulo de subida
+
+DeltaP:float       = Pd(v,h)-Preq(v,h,W)
+DeltaPMax:float    = max(DeltaP) #[W] Maior diferença entre potências
+RCmax:float        = DeltaPMax/W #[m/s] Calcula a razão de subida máxima
+theta_max:float    = max(theta(v,h,W)) #Razão de subida máximo
+LDmax:float        = max(CL(v, h, W)/CD(v, h, W)) #[adim.] Razão L/D máxima
+gammamin:float     = np.arctan(1/LDmax)*180/np.pi #[graus] Definindo o ângulo de planeio 
+RDMAX:float        = max(-v*np.sin(gamma(v,h,m*g)*np.pi/180)) #Razão de planeio mínima
+
+# ---------------------------Funções-----------------------------
+def L_takeoff(v:float, h:int): #[N] Força sustentação da aeronave, função de CL, Q e S
+    return CL_to*q(v,h)*S
+
+def D_takeoff(v:float, h:int):
+    return CD_to*q(v,h)*S
+
+def S_takeoff(h:int, mi:float, m:float) -> np.ndarray:
+    X:list   =[0]
+    V:list   =[0]
+    X0 = V0  = 0
+    i:int    = 0
+    dT:float = 0.005
+    Vstall   = V_stall(h, m)
+    while V[i]<= 1.1*Vstall: #ponto inicial da rotação
+        A = (g/W)*( (Td(V[i], h) - D_takeoff(V[i], h) - mi*( W - L_takeoff(V[i], h))))
+        V.append(V0 + A*dT)
+        X.append(X0 + V[i]*dT)
+        V0:float = V[i+1]
+        X0:float = X[i+1]
+        i=i+1
+    V_array = np.array(V)
+    X_array = np.array(X)
+    return X_array, V_array  
+
+def Sg(trac:callable, h:int, mi:float, m:float, Hw:float) -> np.ndarray:
+    X:list = []
+    X.append(0)
+    X0:float = 0
+    V:list = []
+    V0 = Hw
+    V.append(Hw)
+    i:int = 0
+    dT:float = 0.005
+    W:float = m*9.81
+    Vstall = V_stall(h, m)
+    while V[i]<= Vstall*1.1: #ponto inicial da rotação
+        A = (g/W)*( (trac(V[i], h) - D_takeoff(V[i], h) - mi*( W - L_takeoff(V[i], h))))
+        V.append(V0 + A*dT)
+        X.append(X0 + V[i]*dT)
+        V0:float = V[i+1]
+        X0:float = X[i+1]
+        i=i+1
+    X_array = np.array(X)
+    return X_array[-1]
+
+def MTOW_func(M:float, h:int = 1000, hw:float = 0):
+    return RC(1.1*V_stall(h, M),h,M*9.81) - np.sin(np.arctan(0.7/(55-Sg(Td,h,mi,M, hw))))*1.1*V_stall(h, M)
